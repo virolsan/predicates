22949c456a1d57791ef3c0205a7729735db1abbd
Santtu Virolainen <santtu.virolainen@solita.fi>
2017-05-04 21:46:56 +0300

    answers

--- test/predicates_test.clj
+++ test/predicates_test.clj
@@ -1,2 +1,8 @@
 (ns predicates-test
-  (:use iloveponies.tests.predicates))
+  (:use iloveponies.tests.predicates)
+  (require [predicates :refer :all]
+           [midje.sweet :refer :all]))
+
+(facts "sum-f"
+       (sum-f inc dec 4) => 8)
+
